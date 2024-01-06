b863f48691a5edf4d52d1b0b05595bf76cfb01a3
ashikurbd71 <ashikurovi2003@gmail.com>
2024-01-07 01:30:04 +0600

    project update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # pti.
 
 
-# Technologics used : Tailwind CSS,React JS,React Form Hook,React IcoN
+Technologics used : Tailwind CSS,React JS,React Form Hook,React IcoN
 
 # Live Link : https://pti-food-delivery.netlify.app/
 
--- src/Shared/Container.jsx
+++ src/Shared/Container.jsx
@@ -2,7 +2,7 @@ import React, { Children } from 'react';
 
 const Container = ({children}) => {
     return (
-<div className=' lg:max-w-screen-lg md:max-w-screen-md sm:max-w-screen-sm mx-auto px-5 lg:px-0'>
+<div className=' lg:max-w-[1180px] md:max-w-screen-md sm:max-w-screen-sm mx-auto px-5 lg:px-0'>
             {children}
         </div>
     );
